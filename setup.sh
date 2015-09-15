f9d5f06d38de7ef1f772c6213a06c00923858015
dofuman <o344209s@mail.kyutech.jp>
2015-09-15 23:35:33 +0900

    tex installを追加

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+pwd_dir=$(pwd)
 echo "Installation starts ..."
 #レポジトリの追加
 #gnome-tweak-tool, ubuntu-tweak
@@ -130,5 +131,35 @@ sudo apt-get -yV install cmake-curses-gui
 #speed up compilation tools for PCL
 sudo apt-get install ccache
 sudo apt-get install colorgcc
+
+#Tex install
+# Tex Live Installer のためのパッケージ
+sudo apt-get install -yV perl
+sudo apt-get install -yV perl-tk
+
+cp ./data/install-tl-unx.tar.gz /tmp/
+cd /tmp/
+tar -xvzf /tmp/install-tl-unx.tar.gz
+cd /tmp/install-tl-*
+echo "Start tex installer."
+sudo ./install-tl <<EOF
+I
+EOF
+
+echo "Set Path & Environments"
+
+echo "# TeX Live" >> ~/.bashrc
+echo "export INFOPATH=$INFOPATH:/usr/local/texlive/2015/texmf-dist/doc/info" >> ~/.bashrc
+echo "export MANPATH=$MANPATH:/usr/local/texlive/2015/texmf-dist/doc/man" >> ~/.bashrc
+echo "export PATH=$PATH:/usr/local/texlive/2015/bin/x86_64-linux" >> ~/.bashrc
+cd ${pwd_dir}
+sudo /usr/local/texlive/2015/bin/x86_64-linux/tlmgr path add
+sudo mkdir -p /usr/local/texlive/texmf-local/web2c
+sudo cp ./texmf.cnf /usr/local/texlive/texmf-local/web2c/texmf.cnf
+sudo mktexlsr
+sudo kanji-config-updmap-sys ipaex
+cp latexmkrc ~/.latexmkrc
+echo "Tex Install Finished !!"
+
 
 echo "Installation has been  finished."
