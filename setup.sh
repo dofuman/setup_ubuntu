ad2dcf169bb62d77867209b68832030e6f603419
dofuman <odz70j0z3883d6c@gmail.com>
2015-09-29 19:20:19 +0900

    追加

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get -y update
 sudo apt-get -yV install google-chrome-stable
-n
+
 #更新確認
 sudo apt-get -y update
 sudo apt-get -y upgrade
