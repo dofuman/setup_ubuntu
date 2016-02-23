1a640c96046a8beba00a4466cab2723584aac88f
nishidalab <nishidalab@example.com>
2016-02-23 16:52:49 +0900

    emacs24.5のインストールスクリプト

--- emacs_24.5.sh
+++ emacs_24.5.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
+# emacs24のインストール
 pwd_dir=$(dirname $(readlink -f $0))
-echo "current directory is  $pwd_dir"
 
-echo "こちらでよろしいですか？ [Y/n]"
-read ANSWER
-
-ANSWER=`echo $ANSWER | tr y Y | tr -d '[\[\]]'`
-#echo $ANSWER
-case $ANSWER in
-    ""|Y* ) echo "YES!!";;
-    *  ) echo "NO!!";;
-esac
+sudo apt-get -yV install build-essential
+sudo apt-get -y build-dep emacs24
+cd ~/Downloads
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
+tar -xf emacs-24.5.tar.gz
+cd emacs-24.5
+./configure
+make -j -l
+sudo make install
+cd ~
