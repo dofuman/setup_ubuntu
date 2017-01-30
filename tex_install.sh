eb056a570a4c8e2ddb453369afa3fbba95717412
dofuman <q595202s@mail.kyutech.jp>
2017-01-30 20:52:37 +0100

    modified:   tex_install.sh 16.04対応
    * 16.04対応

--- tex_install.sh
+++ tex_install.sh
@@ -1,61 +1,25 @@
 #!/bin/sh
+
+echo "Start installation"
 pwd_dir=$(cd $(dirname $0); pwd)
+
 #Tex install
-# Tex Live Installer のためのパッケージ
-sudo apt-get install -yV perl
-sudo apt-get install -yV perl-tk
+sudo apt -yV update && sudo apt -yV upgrade
+sudo apt install --assume-yes texlive-lang-cjk
+sudo apt install --assume-yes texlive-fonts-recommended
+sudo apt install --assume-yes texlive-fonts-extra
+sudo apt install --assume-yes xdvik-ja
+sudo apt install --assume-yes dvipsk-ja
+sudo apt install --assume-yes gv
 
-cp ./data/install-tl-unx.tar.gz /tmp/
-cd /tmp/
-tar -xvzf /tmp/install-tl-unx.tar.gz
-cd /tmp/install-tl-*
-echo "Start tex installer."
-sudo ./install-tl <<EOF
-I
-EOF
+# 他文書,図作成用ソフトもインストール
+# .bashrcにexport PATH=$HOME/bin:$PATHを追記するこ.
+sudo apt install 
+mkdir -p ~/bin
+cp script/platex2pdf ~/bin
 
-echo "Set Path & Environments"
-echo "# TeX Live" >> ~/.bashrc
-echo "export INFOPATH=$INFOPATH:/usr/local/texlive/2015/texmf-dist/doc/info" >> ~/.bashrc
-echo "export MANPATH=$MANPATH:/usr/local/texlive/2015/texmf-dist/doc/man" >> ~/.bashrc
-echo "export PATH=\$PATH:/usr/local/texlive/2015/bin/x86_64-linux" >> ~/.bashrc
-cd $pwd_dir
+echo "Please add the following command to ~/.bashrc"
+echo "export PATH=$HOME/bin:$PATH"
 
-sudo /usr/local/texlive/2015/bin/x86_64-linux/tlmgr path add
-sudo mkdir -p /usr/local/texlive/texmf-local/web2c
-sudo cp ./texmf.cnf /usr/local/texlive/texmf-local/web2c/texmf.cnf
-sudo mktexlsr
-sudo kanji-config-updmap-sys ipaex
-cp latexmkrc ~/.latexmkrc
-echo "Tex Install Finished !!"
-echo "Installation has been  finished."
-
-# #Tex install
-# # Tex Live Installer のためのパッケージ
-# sudo apt-get install -yV perl
-# sudo apt-get install -yV perl-tk
-# cd $pwd_dir
-# cp ./data/install-tl-unx.tar.gz /tmp/
-# cd /tmp/
-# tar -xvzf /tmp/install-tl-unx.tar.gz
-# cd install-tl-*
-# echo "Start tex installer."
-# sudo ./install-tl <<EOF
-# I
-# EOF
-
-# echo "Set Path & Environments"
-
-# echo "# TeX Live" >> ~/.bashrc
-# echo "export INFOPATH=$INFOPATH:/usr/local/texlive/2015/texmf-dist/doc/info" >> ~/.bashrc
-# echo "export MANPATH=$MANPATH:/usr/local/texlive/2015/texmf-dist/doc/man" >> ~/.bashrc
-# echo "export PATH=\$PATH:/usr/local/texlive/2015/bin/x86_64-linux" >> ~/.bashrc
-# cd $pwd_dir
-# sudo /usr/local/texlive/2015/bin/x86_64-linux/tlmgr path add
-# sudo mkdir -p /usr/local/texlive/texmf-local/web2c
-# sudo cp ./texmf.cnf /usr/local/texlive/texmf-local/web2c/texmf.cnf
-# sudo mktexlsr
-# sudo kanji-config-updmap-sys ipaex
-# cp latexmkrc ~/.latexmkrc
-# echo "Tex Install Finished !!"
-
+ 
+ 
