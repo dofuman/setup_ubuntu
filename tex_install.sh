eedf832ccc5af9d282ace8a1edf49b0006aba299
dofuman <q595202s@mail.kyutech.jp>
2017-01-31 11:52:11 +0100

    modified: tex_install.sh 他の図表作成用ソフトなどを追加
    * gimp,gnuplotなどを追加

--- tex_install.sh
+++ tex_install.sh
@@ -4,7 +4,7 @@ echo "Start installation"
 pwd_dir=$(cd $(dirname $0); pwd)
 
 #Tex install
-sudo apt -yV update && sudo apt -yV upgrade
+sudo apt -y update && sudo apt -y upgrade
 sudo apt install --assume-yes texlive-lang-cjk
 sudo apt install --assume-yes texlive-fonts-recommended
 sudo apt install --assume-yes texlive-fonts-extra
@@ -13,13 +13,19 @@ sudo apt install --assume-yes dvipsk-ja
 sudo apt install --assume-yes gv
 
 # 他文書,図作成用ソフトもインストール
+sudo apt -yV install nkf
+sudo apt -yV install gnuplot
+sudo apt -yV install gnuplot-x11
+sudo apt -yV install gnuplot-doc
+sudo apt -yV install tgif
+sudo apt -yV install gimp
+sudo apt -yV install inkscape
+sudo apt -yV install mimetex
+sudo apt -yV install latexdiff
 # .bashrcにexport PATH=$HOME/bin:$PATHを追記するこ.
 sudo apt install 
 mkdir -p ~/bin
-cp script/platex2pdf ~/bin
+cp command_script/platex2pdf ~/bin
 
 echo "Please add the following command to ~/.bashrc"
 echo "export PATH=$HOME/bin:$PATH"
-
- 
- 
