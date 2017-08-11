cc1d943eddd7b83fb5ac4dc039cb28f2f428ddd5
dofuman <q595202s@mail.kyutech.jp>
2017-08-11 14:29:14 +0900

    modified: Dropboxのdebファイルを名前をつけて保存するように修正

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sudo apt install google-chrome-stable
 
 # Dropbox
 cd ~/Downloads/
-wget -v https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
+wget -v https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb -O dropbox_2015.10.28_amd64.deb
 sudo dpkg -i dropbox_2015.10.28_amd64.deb
 
 sudo apt update
