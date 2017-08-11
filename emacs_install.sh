f123b8157a6470c72ef5260d8cb3253ed27c31ec
dofuman <q595202s@mail.kyutech.jp>
2017-08-11 13:26:44 +0900

    modified: emacs 25.2に対応

--- emacs_install.sh
+++ emacs_install.sh
@@ -5,12 +5,12 @@ pwd_dir=$(dirname $(readlink -f $0))
 
 # emacs25.1のインストール
 sudo apt -yV install build-essential checkinstall
-sudo apt -y build-dep emacs24 libdbus-1-dev
+sudo apt -y build-dep emacs25 libdbus-1-dev
 cd ~
 mkdir emacs && cd emacs 
-wget http://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.gz
-tar -xf emacs-25.1.tar.gz
-cd emacs-25.1
+wget http://ftp.gnu.org/gnu/emacs/emacs-25.2.tar.gz
+tar -xf emacs-25.2.tar.gz
+cd emacs-25.2
 ./autogen.sh
 ./configure
 make -j $(($(nproc) + 1)) -l $(($(nproc) + 1))
