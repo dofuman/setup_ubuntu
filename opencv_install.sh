39042ba0ea2b7e3cd05c6721cdff058ea5537538
dofuman <q595202s@mail.kyutech.jp>
2017-08-21 13:57:43 +0900

    modified: 微修正 未検証

--- opencv_install.sh
+++ opencv_install.sh
@@ -5,17 +5,17 @@
 CURRENTDIR=$(dirname $(readlink -f $0))
 echo "current directory is $CURRENTDIR"
 
-mkdir -p ~/opencv
-cd ~/opencv
+mkdir /tmp/opencv
+cd /tmp/opencv
 
 echo "Install the dependencies..."
 # Install the dependencies.
-sudo apt-get install --assume-yes build-essential cmake git
-sudo apt-get install --assume-yes build-essential pkg-config unzip ffmpeg qtbase5-dev python-dev python3-dev python-numpy python3-numpy
-sudo apt-get install --assume-yes libopencv-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev
-sudo apt-get install --assume-yes libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install --assume-yes libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev
-sudo apt-get install --assume-yes libvorbis-dev libxvidcore-dev v4l-utils
+sudo apt install build-essential cmake git
+sudo apt install build-essential pkg-config unzip ffmpeg qtbase5-dev python-dev python3-dev python-numpy python3-numpy
+sudo apt install libopencv-dev libgtk-3-dev libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev
+sudo apt install libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt install libv4l-dev libtbb-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev
+sudo apt install libvorbis-dev libxvidcore-dev v4l-utils
 
 # Download source code
 git clone https://github.com/opencv/opencv.git
@@ -27,7 +27,6 @@ make -j $(($(nproc) + 1)) -l $(($(nproc) + 1))
 sudo make install
 sudo /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
-sudo apt update
 
 echo "ALL installation has finished."
 cd $CURRENTDIR
