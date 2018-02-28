c0369f0343dbf55750d11a7f0a9c150914fbc1d4
home-master <home@exmaple.com>
2018-02-28 11:24:26 +0900

    modified: 不足するパッケージのインストールを追加

--- emacs_install.bash
+++ emacs_install.bash
@@ -13,6 +13,10 @@ sudo apt install -y build-essential checkinstall || abort "Please enable dep-src
 # emacs24と25は同じ依存関係を持つ
 sudo apt build-dep emacs24
 
+# 不足するパッケージをインストール
+sudo apt install -y automake autoconf
+sudo apt install -y libgtk2.0-dev libtiff5-dev libgif-dev libjpeg-dev libpng12-dev libxpm-dev libncurses-dev libclang-dev lsb lldb
+
 cd ~
 mkdir emacs && cd emacs 
 wget http://ftp.gnu.org/gnu/emacs/emacs-25.2.tar.gz
@@ -30,4 +34,8 @@ sudo apt  install -y python3-pip
 sudo apt  install -y libclang-dev clang
 sudo pip3 install --upgrade pip
 sudo pip3 install virtualenv
+
+# cask 
+sudo apt install -y curl
+curl -fsSkL https://raw.github.com/cask/cask/master/go | python
 
